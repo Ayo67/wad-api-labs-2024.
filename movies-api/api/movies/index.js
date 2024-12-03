8d8e5d3883c76db80aac1349f58125221c39f402
ayoog <20098135@mail.wit.ie>
2024-12-03 14:50:50 +0000

    Authentication lab - exercise (genres)

--- movies-api/api/movies/index.js
+++ movies-api/api/movies/index.js
@@ -2,6 +2,7 @@ import movieModel from './movieModel';
 import asyncHandler from 'express-async-handler';
 import express from 'express';
 import {getUpcomingMovies} from '../tmdb-api';
+import { getMovieGenres } from '../tmdb-api';
 
 
 const router = express.Router();
@@ -42,5 +43,10 @@ router.get('/tmdb/upcoming', asyncHandler(async (req, res) => {
     const upcomingMovies = await getUpcomingMovies();
     res.status(200).json(upcomingMovies);
 }));
+
+router.get('/tmdb/genres', asyncHandler(async (req, res) => {
+        const genres = await getMovieGenres();
+        res.status(200).json(genres);   
+    }));
 
 export default router;
